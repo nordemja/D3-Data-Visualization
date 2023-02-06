13b150050acc18fc4645d345d4067893841ec7f6
Justin Nordeman <nordemja@mail.uc.edu>
2023-02-05 23:34:46 -0500

    adding class and initial axis for bar chart

--- PROJECTS/PROJECT_1/js/main.js
+++ PROJECTS/PROJECT_1/js/main.js
@@ -3,8 +3,18 @@ d3.csv('data/exoplanets-1.csv')
   	console.log('Data loading complete. Work with dataset.');
     console.log(data);
 
+    data.forEach(d => { //ARROW function - for each object in the array, pass it as a parameter to this function
+        d.sy_snum = +d.sy_snum; // convert string 'sy_snum' to number
 
+    });
+
+    // Initialize chart
+    const barchart = new Barchart({ parentElement: '#barchart'}, data);
+
+    barchart.updateVis()
+    
 })
 .catch(error => {
-    console.error('Error loading the data');
+    console.error(console.error());
 });
+
