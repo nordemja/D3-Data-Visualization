4219b67592c40ed336faa424ebd597a3dfa8fe03
Justin Nordeman <nordemja@mail.uc.edu>
2023-02-12 13:51:17 -0500

    some more charts

--- PROJECTS/PROJECT_1/js/main.js
+++ PROJECTS/PROJECT_1/js/main.js
@@ -3,8 +3,14 @@ d3.csv('data/exoplanets-1.csv')
   	console.log('Data loading complete. Work with dataset.');
     console.log(data);
 
-    starDict = {};
-    star_arr = []
+    let = starDict = {};
+    let = star_arr = [];
+
+    let = planetDict = {};
+    let = planet_arr = [];
+
+    let = discoverDict = {};
+    let = discover_arr = [];
 
     data.forEach(d => { //ARROW function - for each object in the array, pass it as a parameter to this function
         //d.sy_snum = +d.sy_snum; // convert string 'sy_snum' to number
@@ -14,22 +20,64 @@ d3.csv('data/exoplanets-1.csv')
         } else {
             starDict[d.sy_snum] += 1;
         }
+
+        if (planetDict[d.sy_pnum] == undefined) {
+            planetDict[d.sy_pnum] = 1
+        } else {
+            planetDict[d.sy_pnum] += 1;
+        }
+
+        if (discoverDict[d.discoverymethod] == undefined) {
+            discoverDict[d.discoverymethod] = 1
+        } else {
+            discoverDict[d.discoverymethod] += 1;
+        }
     
     });
+
+    console.log(star_arr);
 
     for (let i = 0; i < Object.keys(starDict).length; i++) {
         temp = Object();
         temp.star_num = i+1;
         temp.frequency = starDict[i+1];
         star_arr.push(temp);
-
     }
 
-    console.log(star_arr);
+    console.log(planet_arr);
+
+    for (let i = 0; i < Object.keys(planetDict).length; i++) {
+        temp = Object();
+        temp.star_num = i+1;
+        temp.frequency = planetDict[i+1];
+        planet_arr.push(temp);
+    }
+    
+
+    console.log(discoverDict['Astrometry']);
+
+    
+    for (const property in discoverDict) {
+        temp = Object();
+        temp.star_num = property;
+        temp.frequency = discoverDict[property];
+        discover_arr.push(temp);
+    }
+
+    console.log(discover_arr);
+
 
     // Initialize chart and then show it
     barchart = new Barchart({ parentElement: '#barchart'}, star_arr);
     barchart.updateVis();
+
+    barchart1 = new Barchart({ parentElement: '#barchart1'}, planet_arr);
+    barchart1.updateVis();
+
+    barchart2 = new Barchart({ parentElement: '#barchart2'}, discover_arr);
+    barchart2.updateVis();
+
+    
     
 })
 .catch(error => {
